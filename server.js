87d7515c6b5a39d0658e90c91768e9523dc6ecee
ashraf3013 <asrafash@gmail.com>
2018-02-14 03:45:45 +0530

    after adding talking module

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var data = '';
 var RiveScript = require('./rivescript.min.js');
 var bot;
 
+
+//var zerorpc = require("zerorpc");
+//to make the bot talk
+var shell = require('shelljs');
+
+
 function loadingDone(files) {
     console.log("ok");
     bot.sortReplies();
@@ -30,8 +36,16 @@ app.post("/speech-to-text", function (req, res) {
     req.on("data",function(chunk){
         data = chunk.toString();
         console.log("data from user: "+data);
-        bot = new RiveScript({utf8: true});
-    bot.loadFile("rives/start.rive", loadingDone, loadingError);
+       // bot = new RiveScript({utf8: true});
+    //bot.loadFile("rives/start.rive", loadingDone, loadingError);
+	 var speed = "-s 100";
+        var cmdd = "espeak \""+data+"\" -s 100";
+        console.log("cmd eecuted: "+cmdd);
+        if (shell.exec(cmdd).code !== 0) {
+                 shell.echo('Error: Git commit failed');
+                shell.exit(1);
+}
+
     });
 });
 
